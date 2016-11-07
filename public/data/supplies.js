c9573a96f8379c8382f5fc77ebf505d1798d24b6
Jay Gildea <jg527@kent.ac.uk>
2016-11-07 13:11:37 +0000

    GeoJson data on map

---
+++ public/data/supplies.js
@@ -0,0 +1,20 @@
+eqfeed_callback(
+    {
+        "id": 1,
+        "name": "Canterbury",
+        "food": 400,
+        "water": 200,
+        "gas": 500,
+        "shelter": 400,
+        "coordinates":[126.3832,5.9775,111.16]
+    },
+    {
+        "id": 2,
+        "name": "Margate",
+        "food": 700,
+        "water": 800,
+        "gas": 600,
+        "shelter": 900,
+        "coordinates":[-178.3725,-20.753,544.19]
+    }
+);
