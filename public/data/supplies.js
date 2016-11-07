55e3d7bc6f3218a1d1a99e7c55a5484fe2269025
Jay Gildea <jg527@kent.ac.uk>
2016-11-07 13:23:36 +0000

    Reformated geoJson

--- public/data/supplies.js
+++ public/data/supplies.js
@@ -1,20 +1,29 @@
 eqfeed_callback(
     {
-        "id": 1,
-        "name": "Canterbury",
-        "food": 400,
-        "water": 200,
-        "gas": 500,
-        "shelter": 400,
-        "coordinates":[0,0,0]
+        "type":"FeatureCollection",
+        "features": [
+    {
+        "type": "Feature",
+        "properties": {
+            "id": 1,
+            "name": "Canterbury",
+            "food": 400,
+            "water": 200,
+            "gas": 500,
+            "shelter": 400,
+        },
+        "geometry": {"type": "Point", "coordinates": [126.3832, 5.9775, 111.16]}
     },
     {
-        "id": 2,
-        "name": "Margate",
-        "food": 700,
-        "water": 800,
-        "gas": 600,
-        "shelter": 900,
-        "coordinates":[1,5,5]
+        "type": "Feature",
+        "properties": {
+            id": 2,
+            "name": "Margate",
+            "food": 700,
+            "water": 800,
+            "gas": 600,
+            "shelter": 900,
+        },
+        "geometry": {"type": "Point", "coordinates": [126.3832, 5.9775, 111.16]}
     }
-);
+    ]});
