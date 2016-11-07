093f91e4d507a950962736ad0ff9860fcdffb68a
Jay Gildea <jg527@kent.ac.uk>
2016-11-07 11:53:32 +0000

    attempt at maps

--- app.js
+++ app.js
@@ -25,6 +25,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.get('/supply', function(req, res){
+  getData(function(supply){
+    res.json(supply);
+  });
+});
+
+app.get('/supply/:id', function(req, res){
+    getData(function(supply){
+        res.render('data', {supply: supply.byId[req.params.id], layout:null });
+  });
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
